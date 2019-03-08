a10abfe7f6fb3af9f4f97dc9b46604bb6ae89f3d
tikken <tikken23@gmail.com>
2019-03-08 19:24:55 +0300

    emitter

---
+++ events.js
@@ -0,0 +1,19 @@
+const { EventEmitter } = require('events');
+
+class Kettle extends EventEmitter {
+    start(){
+        setTimeout(() => {
+            this.emit('ready', {
+
+            });
+        })
+    }
+}
+
+const k = new Kettle();
+
+k.start();
+
+k.on('ready', () => {
+    console.log('Чайник вскипел!');
+})
